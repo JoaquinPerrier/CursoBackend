ee61e6640f5b4643bb8f57be743f308209aa3ea9
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2022-11-10 15:07:44 -0300

    Ya se muestran los productos en el front

--- TrabajoFinal/FRONT/app.js
+++ TrabajoFinal/FRONT/app.js
@@ -1,3 +1,37 @@
+const serverUrl = "http://localhost:8080/";
+const itemsPath = "items/";
+const imagesPath = "api/images/";
+
 fetch("http://localhost:8080/api/products")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => mostrarJuegos(data));
+
+function mostrarJuegos(data) {
+  console.log(data.productList);
+  let listadojuegos = document.querySelector("#listadoProductos");
+
+  // lista_productos
+  data.productList.forEach((item) => {
+    listadojuegos.innerHTML += `
+    <div class="col-12 col-lg-6 juegoconteiner" id="juego-${item.id}">
+        <div class="item shadow mb-4" >
+            <h3 class="item-title">${item.title}</h3>
+            <div class="imgdesc">
+            <img class="item-image rounded img-fluid" src=${serverUrl}${imagesPath}${item.imgJuego}>
+            <p>${item.description}</p>
+            </div>
+            <div class="item-details">
+            
+            <div class="precioCadaProd col-6">
+            
+            <span><h4 class="item-price"><u>Precio</u>:</h4><h4>${item.price}  $</h4></span>
+                </div>
+            
+                <div>
+                <button class="item-button btn btn-success" data-toggle="modal" data-target="#modalCompra" onclick="" type="button" >Agregar al carrito!</button>
+                </div>
+            </div>
+        </div>
+    </div>`;
+  });
+}
