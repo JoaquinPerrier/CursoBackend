1fbf5e1c5a617e374ca2bc403e2e4f10ab5ffc05
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2022-11-10 10:02:36 -0300

    Probando cluster

--- Cluster/server.js
+++ Cluster/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const numCPUs = require("os").cpus().length;
 
 // console.log(require('os').cpus());
-//console.log(require('os').cpus().length);
+// console.log(require("os").cpus().length);
 
 //SI NUNCA ABRI UN SERVER ANTES
 if (cluster.isMaster) {
@@ -17,12 +17,36 @@ if (cluster.isMaster) {
     console.log(`MURIO EL WORKER ${worker.process.pid} died`);
   });
 } else {
-  http
+  /*http
     .createServer((req, res) => {
       res.writeHead(200);
       res.end(`GOLPE AL ENDPOINT ${process.pid} !`);
       console.log(`GOLPE AL ENDPOINT ${process.pid} !`);
     })
     .listen(8080);
-  console.log(`INICIE UN Worker NUEVO ${process.pid}`);
+  console.log(`INICIE UN Worker NUEVO ${process.pid}`);*/
+
+  const calculo = () => {
+    let sum = 0;
+    for (let i = 0; i < 6e9; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+  let visitas = 0;
+  const server = http.createServer();
+  server.on("request", (req, res) => {
+    let { url } = req;
+    if (url == "/calcular") {
+      const sum = calculo();
+      res.end(`La suma es ${sum} y estoy en ${process.pid}`);
+    } else if (url == "/") {
+      res.end("Ok " + ++visitas + "y estoy en " + process.pid);
+    }
+  });
+  const PORT = 8080;
+  server.listen(PORT, (err) => {
+    if (err) throw new Error(`Error en servidor: ${err}`);
+    console.log(`Servidor http escuchando en el puerto ${PORT}`);
+  });
 }
