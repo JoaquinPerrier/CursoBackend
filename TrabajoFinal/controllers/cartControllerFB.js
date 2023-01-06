6e5b6643fb05cce1ea7ae2e296b9b0529200725b
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-01-06 20:14:21 -0300

    Update cartControllerFB.js

--- TrabajoFinal/controllers/cartControllerFB.js
+++ TrabajoFinal/controllers/cartControllerFB.js
@@ -1,21 +1,29 @@
 const cartModelFB = require("../models/cartModelFB");
 
 exports.cart_list_FB = async function (req, res) {
+  const userToken = await jwtUtil.checkToken(req);
+
   let cartList = await cartModelFB.findProductsOfCartFB(req, res);
   res.send({ message: "Status OK", cartList: cartList });
 };
 
 exports.create_cart_FB = async function (req, res) {
+  const userToken = await jwtUtil.checkToken(req);
+
   let cartAdded = await cartModelFB.createCartFB(req, res);
   res.send({ message: "Creation OK", cartAdded: cartAdded });
 };
 
 exports.delete_cart_FB = async function (req, res) {
+  const userToken = await jwtUtil.checkToken(req);
+
   let cartDeletedID = await cartModelFB.deleteCartFB(req, res);
   res.send({ message: "Delete OK", cartDeletedID: cartDeletedID });
 };
 
 exports.add_product_to_cart_FB = async function (req, res, productToAdd) {
+  const userToken = await jwtUtil.checkToken(req);
+
   let productAdded = await cartModelFB.addProductToCartFB(
     req,
     res,
@@ -25,6 +33,8 @@ exports.add_product_to_cart_FB = async function (req, res, productToAdd) {
 };
 
 exports.delete_product_from_cart_FB = async function (req, res) {
+  const userToken = await jwtUtil.checkToken(req);
+
   let cartAdded = await cartModelFB.deleteProductFromCartFB(req, res);
   res.send({ message: "Product deleted OK", cartAdded: cartAdded });
 };
